197926cfee7fe3cdc97b0eb4e1f502dd9800a595
hroft <sagdinov@gmail.com>
2019-02-25 17:04:20 +0300

    add css-flex and img

--- src/Components/ContentItems/ContentItems.js
+++ src/Components/ContentItems/ContentItems.js
@@ -7,11 +7,15 @@ export default function ContentItems(props){
         <React.Fragment>
             <div className="wraper">
           {props.data.map(row => (
+              
                 //   <h5 className="card-title">{row.date}</h5>
 /* <div key={row.id} dangerouslySetInnerHTML={{ __html: row.excerpt.rendered}} > */
-<div key={row.id}>
-<div key={row.id} dangerouslySetInnerHTML={{ __html: row.excerpt.rendered}} />
-<a href="">Подробнее</a>
+<div key={row.id} className="card">
+<div className="cardItem">
+<img src={row._embedded['wp:featuredmedia']['0'].media_details.sizes.medium.source_url} alt="" />
+</div>
+<div dangerouslySetInnerHTML={{ __html: row.excerpt.rendered}} />
+<a href="#">Подробнее</a>
 
 </div>
 
